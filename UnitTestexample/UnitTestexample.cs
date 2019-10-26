a8304cae600e01a9402f4d53515ff8b4e46ac300
studenti <studenti@rosselli.loc>
2019-10-26 12:07:14 +0200

    Add project files.

---
+++ UnitTestexample/UnitTestexample.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestexample
+{
+    [TestClass]
+    public class UnitTestexample
+    {
+        [TestMethod]
+        public void Calcolo1Test()
+        {
+            
+        }
+    }
+}
